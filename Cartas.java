3db7352e94548fdb4a3e599d515de343ad687bdf
SilenceSounds <70463457+SilenceSounds@users.noreply.github.com>
2020-10-10 10:33:17 -0500

    Interfaz Grafica añadida
    Se ha implementado una interfaz gráfica valiendose de la consola.

--- Cartas.java
+++ Cartas.java
@@ -1,25 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cartas;
-
+import java.util.Scanner;
 /**
  *
  * @author Sayad
  */
 public class Cartas {
+    
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        
+       Scanner scanner = new Scanner(System.in);
        Deck NewDeck = new Deck();
-       NewDeck.DeckInit();
-       NewDeck.Hand();
-       
+       NewDeck.DeckInit(); 
+       boolean End = false; 
+        
+        do{
+            System.out.println("Bienvenido a Poker!");
+            System.out.println("1 Mezclar deck");
+            System.out.println("2 Sacar una carta");
+            System.out.println("3 Carta al azar");
+            System.out.println("4 Generar una mano de 5 cartas");
+            System.out.println("0 Salir \n");
+            
+            System.out.println("Opcion Elegida: ");
+            
+            int E = scanner.nextInt();
+            
+            System.out.println();
+            
+            switch (E){
+                case 1:
+                    NewDeck.Shuffle();
+                    break;
+                    
+                case 2:
+                    NewDeck.Head();
+                    break;
+                    
+                case 3:
+                    NewDeck.Pick();
+                    break;
+                    
+                case 4:
+                    NewDeck.Hand();
+                    break;
+                    
+                case 0:
+                    End = true;
+                    break;
+                
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+                    
+            }
+            
+            System.out.println("\n");
+
+        }while(End != true);
+         
        
     }
+    
     
 }
